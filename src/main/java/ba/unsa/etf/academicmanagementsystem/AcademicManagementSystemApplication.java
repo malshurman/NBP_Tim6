0d647696c8fcec5c3d811684e6ef6fd3d2cab551
malshurman <Muhammed.Al-shurman@infobip.com>
2025-04-01 22:37:12 +0200

    Initial commit of academic-management-system

---
+++ src/main/java/ba/unsa/etf/academicmanagementsystem/AcademicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.academicmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ba/unsa/etf/academicmanagementsystem/AcademicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.academicmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
